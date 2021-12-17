0b210e16df49e9691787e2a79a319a13f42600d6
GODCREATOR333 <preetham.chitti.9@gmail.com>
2021-12-17 23:19:28 +0530

    void pointers

--- Pointers---c.c
+++ Pointers---c.c
@@ -3,25 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <conio.h>
 
-
-int main ()
+int main()
 {
-int b = 10 ;
-char c[10] = "chad" ;
-int a = 5;
-int *pa = &a;
-char *pc= &c;
-int *pb = &b;
-printf("%p\n",&b);
-printf("%p\n",&c) ;
-printf("%p\n",&a);
-
-printf("%d\n" , *&b);
-printf("%s\n", *&c);
-printf("%d\n", *&a);
-
-
-return 0 ; 
-
+    int a = 99 ;
+    int* p;
+    p = &a;
+    //void pointer -- generic pointer type 
+    void *z;
+    z = p;
+    printf("address = %d ",z);
+    
+    
+    return 0 ;
 }
